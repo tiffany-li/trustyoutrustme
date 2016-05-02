306985afa414fc542cca3d8b3223b4a89473da69
Tiffany Li <tl476@cornell.edu>
2016-05-02 19:31:25 -0400

    php test

--- gallery.php
+++ gallery.php
@@ -1,3 +1,39 @@
+<?php
+    if (isset($_POST["submit"])) {
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $human = intval($_POST['human']);
+        $from = 'trustyoutrustme'; 
+        $to = 'balafafa@gmail.com'; 
+        $subject = 'Message from trustmetrustyou';
+        
+        $body = "From: $email\n Message:\n $message";
+        
+        // Check if email has been entered and is valid
+        if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errEmail = 'Please enter a valid email address';
+        }
+        
+        //Check if message has been entered
+        if (!$_POST['message']) {
+            $errMessage = 'Please enter your message';
+        }
+        //Check if simple anti-bot test is correct
+        if ($human !== 5) {
+            $errHuman = 'Your anti-spam is incorrect';
+        }
+ 
+// If there are no errors, send the email
+if (!$errEmail && !$errMessage && !$errHuman) {
+    if (mail ($to, $subject, $body, $from)) {
+        $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+    } else {
+        $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+    }
+}
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -32,7 +68,7 @@
 
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
     <!-- Intro Section -->
-    <a href="index.php">
+    <a href="index.html">
         <span class="glyphicon glyphicon-home backbutton"></span>
     </a>
     <section id="myCarousel" class="carousel slide">
@@ -123,7 +159,7 @@
             <div class="row">
                 <h1 style="padding-bottom: 25px"> Share a Story </h1>
                 <div class="col-md-12" style="left:-95px">
-                    <form class="form-horizontal" role="form" method="post" action="index.php">
+                    <form class="form-horizontal" role="form" method="post" action="gallery.php">
                         <div class="form-group">
                             <label for="email" class="col-sm-2 control-label">Email</label>
                             <div class="col-sm-10">
